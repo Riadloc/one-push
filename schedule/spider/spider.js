d40d0767374699df3b4c769dbe4b6f720bb597b6
ywang <youngwangyang_v@didiglobal.com>
2021-06-03 11:08:57 +0800

    🐛

--- schedule/spider/spider.js
+++ schedule/spider/spider.js
@@ -15,10 +15,10 @@ async function spider() {
     for (const item of gzh) {
       const { id, name } = item;
       await page.goto(`${url}${id}`);
-      await page.waitForTimeout(1000);
+      await page.waitForTimeout(3000);
       const linkHref = await page.$eval('.news-box ul li dl:nth-last-of-type(1) a', el => el.href);
       await page.goto(linkHref);
-      await page.waitForNavigation();
+      await page.waitForTimeout(3000);
       await page.waitForSelector('#activity-detail');
       const tilte = await page.$eval('#activity-name', el => el.innerText);
       const contents = await page.content();
@@ -29,12 +29,12 @@ async function spider() {
         console.log(error);
       }
       await fs.writeFile(path.join(dir, `/[${name}] ${tilte}.html`), contents, 'utf-8');
-      await page.waitForTimeout(1000);
+      await page.waitForTimeout(3000);
     }
-    await browser.close();
   } catch (error) {
     console.error(error);
   }
+  await browser.close();
 }
 
 module.exports = spider;
