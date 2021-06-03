a652afe32290627f7f51f5c691d8da7ed5af6dbc
ywang <youngwangyang_v@didiglobal.com>
2021-06-03 11:14:30 +0800

    🐛

--- schedule/spider/spider.js
+++ schedule/spider/spider.js
@@ -12,22 +12,23 @@ async function spider() {
   const page = await browser.newPage();
   try {
     const url = 'https://weixin.sogou.com/weixin?type=1&query=';
+    const dir = path.join(__dirname, `/${dayjs(new Date()).format('YYYY-MM-DD')}`);
+    try {
+      await fs.mkdir(dir);
+    } catch (error) {
+      console.log(error);
+    }
     for (const item of gzh) {
       const { id, name } = item;
       await page.goto(`${url}${id}`);
       await page.waitForTimeout(3000);
+      await page.screenshot({path: path.join(dir, `/${name} screenshot.png`)});
       const linkHref = await page.$eval('.news-box ul li dl:nth-last-of-type(1) a', el => el.href);
       await page.goto(linkHref);
       await page.waitForTimeout(3000);
       await page.waitForSelector('#activity-detail');
       const tilte = await page.$eval('#activity-name', el => el.innerText);
       const contents = await page.content();
-      const dir = path.join(__dirname, `/${dayjs(new Date()).format('YYYY-MM-DD')}`);
-      try {
-        await fs.mkdir(dir);
-      } catch (error) {
-        console.log(error);
-      }
       await fs.writeFile(path.join(dir, `/[${name}] ${tilte}.html`), contents, 'utf-8');
       await page.waitForTimeout(3000);
     }
